0f27a5ec047224c9dd2fd2d0e87ad31ad2733f81
trung674 <thanhtrung674@gmail.com>
2017-07-05 09:45:25 +0700

    add heroku config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'class_db'
-});
+var mysql = require('mysql');
 
+if (process.env.JAWSDB_MARIA_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+} else {
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'class_db'
+  });
+}
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
